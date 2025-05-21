2e27bfd2fb6b48e066e263ae6e1c7a1e46715124
Szilveszter Milán <szmilan000@gmail.com>
2025-05-21 17:26:01 +0200

    Added more visualizations.

--- streamlit_evidentlyai/monitor_with_streamlit_train_data.py
+++ streamlit_evidentlyai/monitor_with_streamlit_train_data.py
@@ -1,48 +1,62 @@
 import streamlit as st
-import streamlit.components.v1 as components
 import pandas as pd
-from evidently import ColumnMapping
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-from evidently.report import Report
-from evidently.metrics.base_metric import generate_column_metrics
-from evidently.metric_preset import DataDriftPreset, TargetDriftPreset, DataQualityPreset, RegressionPreset
-from evidently.metrics import *
-
-from evidently.test_suite import TestSuite 
-from evidently.tests.base_test import generate_column_tests
-from evidently.test_preset import DataStabilityTestPreset, NoTargetPerformanceTestPreset
-from evidently.tests import *
+# st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.markdown("# Train Dataset ")
 st.sidebar.markdown("# Train Dataset ")
 
 def get_raw_data():
-    
     df_train = pd.read_csv('data/crop_train.csv')
     df_test = pd.read_csv('data/crop_test.csv')
-    experiment_name = '1'
-    
     return df_train, df_test
 
 header = st.container()
 dataset = st.container()
 plot_area_code = st.container()
+feature_distributions = st.container()
+correlation_heatmap = st.container()
+pairwise_relationship = st.container()
 
 with header:
-    st.title('Monitoring some elements')
-    st.text("You can see some examples of Streamlit'possibilities")
+    st.title('Crop Recommendation Dataset Monitoring')
+    st.text("A dashboard exploring agricultural input data.")
 
 with dataset:
-    st.header("Crop dataset import")
-    st.text("You can see here a sample from the train dataset")
-    
+    st.header("Train Dataset Sample")
     df_train, df_test = get_raw_data()
-    st.write(df_train.head(20))
-
 
 with plot_area_code:
-    st.header("Plot label value counts")
-    total_day_minutes = df_train["label"].value_counts().head(50)
-    st.bar_chart(total_day_minutes)
+    st.header("Label Distribution (Most Suitable Crops)")
+    label_counts = df_train["label"].value_counts()
+    st.bar_chart(label_counts)
+
+with feature_distributions:
+    st.header("Feature Distributions")
+    numeric_features = ["N", "P", "K", "temperature", "humidity", "ph", "rainfall"]
+
+    selected_feature = st.selectbox("Choose a feature to visualize its distribution", numeric_features)
+    fig, ax = plt.subplots()
+    sns.histplot(df_train[selected_feature], kde=True, bins=30, ax=ax)
+    st.pyplot(fig)
+
+with correlation_heatmap:
+    st.header("Correlation Heatmap")
+    corr = df_train[numeric_features].corr()
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.heatmap(corr, annot=True, cmap="coolwarm", ax=ax)
+    st.pyplot(fig)
+
+with pairwise_relationship:
+    st.header("Pairwise Feature Relationships")
+    st.text("Colored by crop label (might be slow on large datasets)")
+    selected_features = st.multiselect("Select up to 4 features for pairplot", numeric_features, default=numeric_features[:3])
+    if len(selected_features) >= 2:
+        fig = sns.pairplot(df_train, vars=selected_features, hue="label", diag_kind="kde", corner=True)
+        st.pyplot(fig)
+    else:
+        st.warning("Please select at least two features.")
 
 # streamlit run monitor_with_streamlit_train_data.py
